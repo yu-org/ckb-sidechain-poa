99863d84ad16554cbfb55b5d44859e07886911df
Lawliet-Chan <1576710154@qq.com>
2022-04-23 16:24:12 +0800

    complete main

--- main.go
+++ main.go
@@ -3,9 +3,36 @@ package main
 import (
 	"github.com/yu-org/yu/apps/asset"
 	"github.com/yu-org/yu/apps/poa"
+	"github.com/yu-org/yu/core/keypair"
 	"github.com/yu-org/yu/core/startup"
+	"os"
+	"strconv"
 )
 
 func main() {
-	startup.StartUp(poa.NewPoa(), asset.NewAsset("yu-coin"), NewSidechain())
+	idxStr := os.Args[1]
+	idx, err := strconv.Atoi(idxStr)
+	if err != nil {
+		panic(err)
+	}
+
+	pubkey, privkey, validators := poa.InitDefaultKeypairs(idx)
+
+	ckbUrl := "http://127.0.0.1:8114"
+	lastCkbTxHash := []byte{}
+
+	var pubkeys []keypair.PubKey
+	var otherIps []string
+	for i, validator := range validators {
+		pubkeys = append(pubkeys, validator.Pubkey)
+		if i != idx {
+			otherIps = append(otherIps, validator.P2pIP)
+		}
+	}
+
+	startup.StartUp(
+		poa.NewPoa(pubkey, privkey, validators),
+		asset.NewAsset("yu-coin"),
+		NewSidechain(ckbUrl, pubkeys, privkey, otherIps, lastCkbTxHash),
+	)
 }
